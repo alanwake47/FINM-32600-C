64cd18161fec1cdbdac3f38109f1e7e22e82601b
Aman Krishna <aman.krishna4@gmail.com>
2023-01-06 18:53:00 -0600

    v0.1

--- temp.cpp
+++ temp.cpp
@@ -2,5 +2,10 @@
 
 int main() {
     std::cout << "Hello, World Again!" << std::endl;
+    //write a function to add two numbers
+    int a = 5;
+    int b = 6;
+    int c = a + b;
+    std::cout << "The sum of a and b is " << c << std::endl;
     return 0;
 }
