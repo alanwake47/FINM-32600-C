a1fdfb0b1578ec216c0c44cf5f7d13c6b5c4598a
Aman Krishna <aman.krishna4@gmail.com>
2023-01-10 22:19:36 -0600

    hw0

--- hw0/main.cpp
+++ hw0/main.cpp
@@ -1,6 +1,20 @@
+/*
+Problem:
+1. Write a function to add two integers.
+2. Read any two integer values from the keyboard. 3. Add them using the function you wrote.
+4. Write the result to console.*/
+
 #include <iostream>
+#include <string>
+
+int add(int a, int b) {
+    return a + b;
+}
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    int a, b;
+    std::cout << "Enter two integers: ";
+    std::cin >> a >> b;
+    std::cout << "The sum of " << a << " and " << b << " is " << add(a, b) << std::endl;
     return 0;
 }
