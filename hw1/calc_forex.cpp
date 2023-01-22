6338c7312d3516e5010bd0eb45792b6506eb56e5
Aman Krishna <aman.krishna4@gmail.com>
2023-01-21 18:26:50 -0600

    hw1_final

--- hw1/calc_forex.cpp
+++ hw1/calc_forex.cpp
@@ -5,5 +5,5 @@
 // calculate the amount of foreign currency you can buy
 
 float calc_forex(float base, float rate_b, float rate_f) {
-    return (base * rate_b) / rate_f;
+    return (base * rate_f) / rate_b;
 }
